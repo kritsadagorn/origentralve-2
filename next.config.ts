ff3d5c7fe60ad4a26f217fb12ac002b05040b293
krit_hype <catsamotyba@gmail.com>
2025-10-16 16:13:16 +0700

    eslint

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
-
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default withNextIntl(nextConfig);
